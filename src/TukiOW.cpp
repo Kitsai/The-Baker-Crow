afb8ae30cda3497097a13a473ad63fbbea4aed34
Kitsai <lucas.rocha.3801@gmail.com>
2023-10-25 15:52:48 -0300

    directional dash

--- src/TukiOW.cpp
+++ src/TukiOW.cpp
@@ -18,17 +18,28 @@ void TukiOW::Update(float dt) {
     if(iM.KeyPress(Z_KEY) && state != PlayerState::DODGING) {
         //Sets the state to dodging sets the speed and deactivates the hitbox.
         state = PlayerState::DODGING;
-        speed = speed.normalized() * TOW_DASH_SPEED;
+
+        Vec2 direction;
+        if(iM.IsKeyDown(UP_ARROW_KEY) || iM.IsKeyDown(DOWN_ARROW_KEY) || iM.IsKeyDown(LEFT_ARROW_KEY) || iM.IsKeyDown(RIGHT_ARROW_KEY)) {
+            if(iM.IsKeyDown(UP_ARROW_KEY)) direction.y -= 1;
+            if(iM.IsKeyDown(DOWN_ARROW_KEY)) direction.y += 1;
+            if(iM.IsKeyDown(LEFT_ARROW_KEY)) direction.x -= 1;
+            if(iM.IsKeyDown(RIGHT_ARROW_KEY)) direction.x += 1;   
+        } 
+        else
+            direction = speed;
+
+        speed = direction.normalized() * TOW_DASH_SPEED;
 
         Collider* hitbox = (Collider*)associated.GetComponent("Collider");
         hitbox->active = false;
         hitbox->SetColor(COLOR_BLUE);
     }
 
-        // simulating the calculation of the speed integral by separating the calculation in two steps.
-        CalcSpeed(dt);
-        associated.box += speed*dt;
-        CalcSpeed(dt);
+    // simulating the calculation of the speed integral by separating the calculation in two steps.
+    CalcSpeed(dt);
+    associated.box += speed*dt;
+    CalcSpeed(dt);
 
     if(state == PlayerState::DODGING) {
         dashTimer.Update(dt);
