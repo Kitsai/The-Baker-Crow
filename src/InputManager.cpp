06393e639b202addcc3a6f1bede787fb34c89cb5
Eduardo Marciano <89046882+EduardoMarciano@users.noreply.github.com>
2023-11-02 11:42:15 -0300

    fix: Duplicated declaration ID on Update's switch

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -88,59 +88,78 @@ void InputManager::Update() {
     updateCounter++;
 
     while(SDL_PollEvent(&event)) {
-
-        switch (event.type)
-        {
-        case SDL_QUIT:
-            quitRequested = true;
-            break;
-        
-        case SDL_MOUSEBUTTONDOWN:
-            mouseState[event.button.button] = true;
-            mouseUpdate[event.button.button] = updateCounter;
-            break;
-
-        case SDL_MOUSEBUTTONUP:
-            mouseState[event.button.button] = false;
-            mouseUpdate[event.button.button] = updateCounter;
-            break;
-
-        case SDL_KEYDOWN:
-            if(event.key.repeat != 1) break;
-            int id = event.key.keysym.sym;
-            if(id >= 0x40000000) id -= 0x3FFFFF81;
-
-            keyState[id] = true;
-            keyUpdate[id] = updateCounter;
-            break;
-
-        case SDL_KEYUP:
-            if(event.key.repeat != 1) break;
-            int id = event.key.keysym.sym;
-            if(id >= 0x40000000) id -= 0x3FFFFF81;
-
-            keyState[id] = false;
-            keyUpdate[id] = updateCounter;
-            break;
-
-        case SDL_CONTROLLERDEVICEADDED:
-            if(!controller) controller = new Controller((int)event.cdevice.which);
-            break;
-        case SDL_CONTROLLERDEVICEREMOVED:
-            if(controller && event.cdevice.which == controller->GetId()) {
-                delete controller;
-                controller = Controller::CheckControllers();
+        int id;
+        switch (event.type) {
+            case SDL_QUIT:
+                quitRequested = true;
+                break;
+            
+            case SDL_MOUSEBUTTONDOWN: {
+                mouseState[event.button.button] = true;
+                mouseUpdate[event.button.button] = updateCounter;
+                break;
             }
-            break;
-        case SDL_CONTROLLERBUTTONDOWN:
-        case SDL_CONTROLLERBUTTONUP:
-            if(controller && event.cdevice.which == controller->GetId()) {
-                controller->ProcessEvent(event);
-            }
-            break;
 
-        default:
-            break;
+            case SDL_MOUSEBUTTONUP: {
+                mouseState[event.button.button] = false;
+                mouseUpdate[event.button.button] = updateCounter;
+                break;
+            }
+
+            case SDL_KEYDOWN: {
+                if(event.key.repeat != 1) {
+                    break;
+                }
+                id = event.key.keysym.sym;
+                if(id >= 0x40000000) {
+                    id -= 0x3FFFFF81;
+                }
+
+                keyState[id] = true;
+                keyUpdate[id] = updateCounter;
+                break;
+            }
+
+            case SDL_KEYUP: {
+                if(event.key.repeat != 1) {
+                    break;
+                }
+                id = event.key.keysym.sym;
+                if(id >= 0x40000000) {
+                    id -= 0x3FFFFF81;
+                }
+
+                keyState[id] = false;
+                keyUpdate[id] = updateCounter;
+                break;
+            }
+
+            case SDL_CONTROLLERDEVICEADDED: {
+                if(!controller) {
+                    controller = new Controller((int)event.cdevice.which);
+                }
+                break;
+            }
+
+            case SDL_CONTROLLERDEVICEREMOVED: {
+                if(controller && event.cdevice.which == controller->GetId()) {
+                    delete controller;
+                    controller = Controller::CheckControllers();
+                }
+                break;
+            }
+
+            case SDL_CONTROLLERBUTTONDOWN:
+            case SDL_CONTROLLERBUTTONUP: {
+                if(controller && event.cdevice.which == controller->GetId()) {
+                    controller->ProcessEvent(event);
+                }
+                break;
+            }
+
+            default:
+                break;
         }
-    }            
+    }
+         
 }
