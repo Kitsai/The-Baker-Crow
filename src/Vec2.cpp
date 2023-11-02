74ae3d34bcf32e116b606bd943a7f95c46469236
Eduardo Marciano <89046882+EduardoMarciano@users.noreply.github.com>
2023-11-02 16:52:53 -0300

    fix && refactor: character deleted on dash
    Avoiding division by zero that was causing character deletion during dash casting in Vec2.cpp and diminishing the size of the class.

--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -1,53 +1,42 @@
 #include "Vec2.h"
 #include <cmath>
 
-
 Vec2::Vec2(float x, float y) {
     this->x = x;
     this->y = y;
 }
 
 void Vec2::operator = (const Vec2& v) {
-    x = v.x;
-    y = v.y;
+    this->x = v.x;
+    this->y = v.y;
 }
 
 Vec2 Vec2::operator + (const Vec2& v) {
-    Vec2 vec;
-    vec.x = x + v.x;
-    vec.y = y + v.y;
-    return vec;
+    return Vec2(this->x +v.x,this->y+ v.y);
 }
+
 Vec2 Vec2::operator - (const Vec2& v) {
-    Vec2 vec;
-    vec.x = x - v.x;
-    vec.y = y - v.y;
-    return vec;
+    return Vec2(this->x - v.x, this->y - v.y);
 }
+
 void Vec2::operator += (const Vec2& v) {
-    x += v.x;
-    y += v.y;
+    this->x += v.x;
+    this->y += v.y;
 }
 void Vec2::operator -= (const Vec2& v) {
-    x -= v.x;
-    y -= v.y;
+    this->x -= v.x;
+    this->y -= v.y;
 }
-Vec2 Vec2::operator * (float e) {
-    Vec2 vec;
-    vec.x = x*e;
-    vec.y = y*e;
-    return vec;
+Vec2 Vec2::operator*(float scalar){
+    return Vec2(this->x * scalar, this->y * scalar);
 }
 
 Vec2 Vec2::operator / (float e) {
-    Vec2 vec;
-    vec.x = x/e;
-    vec.y = y/e;
-    return vec;
+    return Vec2(this->x/e, this->y/e);
 }
 
 bool Vec2::operator==(const Vec2& v) {
-    return x==v.x && y==v.y;
+    return this->x==v.x && this->y==v.y;
 }
 
 bool Vec2::operator!=(const Vec2& v) {
@@ -92,20 +81,24 @@ Vec2 Vec2::normalized() {
     //float inv = calcInvRoot(x*x + y*y);
     //ret.x = x * inv;
     //ret.y = y * inv;
-
     float mag = magnitude();
-    ret.x = x/mag;
-    ret.y = y/mag;
-
+    if (mag != 0) {
+        ret.x = this->x / mag;
+        ret.y = this->y / mag;
+    } else {
+        ret.x = this->x;
+        ret.y = this->y;
+    }
     return ret;
 }
+
 
 float Vec2::distVec2(const Vec2& b) {
     return (*this-b).magnitude();
 }
 
 float Vec2::incl() {
-    return atan2(y,x);
+    return atan2(this->y,this->x);
 }
 
 float Vec2::inclVec2(const Vec2& b) {
