0e62a2eb71d82f603f7f62a0b327112a067f2ff6
Marcolino5 <102527399+Marcolino5@users.noreply.github.com>
2023-12-09 20:03:17 -0300

    feat: added bakery state floors

--- src/TukiB.cpp
+++ src/TukiB.cpp
@@ -1,8 +1,16 @@
 #include "TukiB.h"
+#include "Sprite.h"
+#include "states/BakeryState.h"
 
 TukiB::TukiB(GameObject& associated): Player(associated) {
-    associated.AddComponent(new Sprite(associated, "resources/img/try.png"));
     floor = 0;
+
+    Sprite*  sprite = new Sprite(associated, "resources/img/Tuki_idle_front.png");
+    sprite->SetScale(2.0f,2.0f);
+    associated.AddComponent(sprite);
+    faceDirection = DOWN;
+
+    Talk("resources/img/menu_button.png", 1.0f);
 }
 
 TukiB::~TukiB() {
@@ -12,58 +20,19 @@ TukiB::~TukiB() {
 void TukiB::Update(float dt) {
     InputManager& iM = InputManager::GetInstance();
 
-    if(hp <= 0) {
-        associated.RequestDelete();
-        GameData::playerAlive = false;
-    }
-
-    if(iM.KeyPress(Z_KEY) && Player::GetPlayerState() != PlayerState::DODGING) {
-        //Sets the state to dodging sets the speed and deactivates the hitbox.
-        Player::SetPlayerState(PlayerState::DODGING);
-
-        Vec2 direction;
-        if(iM.IsKeyDown(UP_ARROW_KEY) || iM.IsKeyDown(DOWN_ARROW_KEY) || iM.IsKeyDown(LEFT_ARROW_KEY) || iM.IsKeyDown(RIGHT_ARROW_KEY)) {
-            if(iM.IsKeyDown(UP_ARROW_KEY)) direction.y -= 1;
-            if(iM.IsKeyDown(DOWN_ARROW_KEY)) direction.y += 1;
-            if(iM.IsKeyDown(LEFT_ARROW_KEY)) direction.x -= 1;
-            if(iM.IsKeyDown(RIGHT_ARROW_KEY)) direction.x += 1;   
-        } 
-        else
-            direction = speed;
-
-        speed = direction.normalized() * TOW_DASH_SPEED;
-
-        Collider* hitbox = (Collider*)associated.GetComponent("Collider").lock().get();
-        hitbox->active = false;
-        hitbox->SetColor(COLOR_BLUE);
-    }
-
     // simulating the calculation of the speed integral by separating the calculation in two steps.
     Move(dt);
     
-    if(Player::GetPlayerState() == PlayerState::DODGING) {
-        //playerTimer.Update(dt);
-        if(speed.magSquare() < TOW_SPEED_LIM*TOW_SPEED_LIM) {
-            speed = speed.normalized()*TOW_SPEED_LIM;
-            Player::SetPlayerState(PlayerState::WALKING);
-            //playerTimer.Restart();
-
-            Collider* hitbox = (Collider*)associated.GetComponent("Collider").lock().get();
-            hitbox->active = true;
-            hitbox->SetColor(COLOR_RED);
+     // Switches from Walking to Standing.
+    if(speed.magnitude() < 1) {
+        if(GetPlayerState() == WALKING) SetPlayerState(STANDING);
+        speed = {0,0};
         }
-    }
-    else {
-        // Switches from Walking to Standing.
-        if(speed.magnitude() < 1) {
-            Player::SetPlayerState(PlayerState::STANDING);
-            speed = {0,0};
-        }
-        else Player::SetPlayerState(PlayerState::WALKING);
-    }
+        else if(GetPlayerState() == STANDING) SetPlayerState(WALKING);
 } 
 
 void TukiB::Move(float dt) {
+
     CalcSpeed(dt);
     // std::cout << speed.magnitude() << std::endl;
     if(Player::GetPlayerState() == PlayerState::STANDING) {
@@ -78,7 +47,7 @@ void TukiB::Move(float dt) {
 void TukiB::CalcSpeed(float dt) {
     InputManager& iM = InputManager::GetInstance();
 
-    if(Player::GetPlayerState() == PlayerState::DODGING)
+    if(GetPlayerState() == DODGING)
         return;
 
     if(iM.IsKeyDown(LEFT_ARROW_KEY)) {
@@ -89,12 +58,32 @@ void TukiB::CalcSpeed(float dt) {
     }
     if(iM.IsKeyDown(UP_ARROW_KEY)) {
         speed.y -= TOW_A*dt;
+        if(GetPlayerState() == WALKING && faceDirection == DOWN) {
+            ChangeSprite("resources/img/Tuki_anim_costas.png",8,.2F);
+            faceDirection = UP;
+        }
+        if (floor == 1 && associated.box.x < 300 && associated.box.x > 90 && associated.box.y < 0) {
+            floor = 0;
+            associated.box.x = 240;
+            associated.box.y = 530;
+        }
     }
     if(iM.IsKeyDown(DOWN_ARROW_KEY)) {
         speed.y += TOW_A*dt;
+        if(GetPlayerState() == WALKING && faceDirection == UP) {
+            ChangeSprite("resources/img/Tuki_anim2.png",8,.2F);
+            faceDirection = DOWN;
+        }
+        if (floor == 0 && associated.box.x < 260 && associated.box.x > 70 && associated.box.y > 530) {
+            floor = 1;
+            associated.box.x = 240;
+            associated.box.y = 0;
+        } else if (floor == 1  && associated.box.x < 600 && associated.box.x > 500 && associated.box.y > 530) {
+            floor = 2;
+        }
     }
     Vec2 norm = speed.normalized();
-    if( Player::GetPlayerState() != PlayerState::DODGING && speed.magnitude() > TOW_SPEED_LIM) 
+    if( speed.magnitude() > TOW_SPEED_LIM) 
         speed = norm * TOW_SPEED_LIM;
 }
 
@@ -108,4 +97,48 @@ void TukiB::SetFloor(int floor) {
 
 int TukiB::GetFloor() {
     return floor;
+}
+
+void TukiB::SetPlayerState(PlayerState state) {
+    Player::SetPlayerState(state);
+
+    switch (state)
+    {
+    case STANDING:
+        if(faceDirection == DOWN)
+            ChangeSprite("resources/img/Tuki_idle_front.png",1,1);
+        else if(faceDirection == UP)
+            ChangeSprite("resources/img/Tuki_idle_costas.png",1,1);
+        break;
+    case WALKING:
+        if(faceDirection == DOWN)
+            ChangeSprite("resources/img/Tuki_anim2.png",8,.2F);
+        else if(faceDirection == UP)
+            ChangeSprite("resources/img/Tuki_anim_costas.png",8,.2F);
+        break;
+    case ATTACKING:
+        ChangeSprite("resources/img/tuki_anim_attac.png",4,.1F);
+        SetCollider(COLOR_GREEN);
+        break;
+    case DODGING:
+        ChangeSprite("resources/img/try.png",1,1);
+        break;
+    case DAMAGED:
+        ChangeSprite("resources/img/tuki_anim_dano.png",4,.15F);
+        break;
+    default:
+        break;
+    }
+}
+
+void TukiB::Talk(std::string file, float time){
+    SetPlayerState(STANDING);
+
+    GameObject* speakBalloon = new GameObject();
+    Sprite* sprite = new Sprite(*speakBalloon, "resources/img/MenuButton.png");
+    speakBalloon->AddComponent(sprite);
+    speakBalloon->box.x = 0;
+    speakBalloon->box.y = 0;
+
+
 }
--- src/states/BakeryState.cpp
+++ src/states/BakeryState.cpp
@@ -1,14 +1,24 @@
 #include "states/BakeryState.h"
 #include "Game.h"
 #include "TukiB.h"
+#include "states/ResumeState.h"
+#include "states/OverworldState.h"
 
 BakeryState::BakeryState() : State() {
     floor = 0;
 
-    GameObject* bedroom = new GameObject();
-    bedroom->AddComponent(new Sprite(*bedroom, "resources/img/bedroom_ph.png"));
-    bedroom->box.SetCenter({Game::GetInstance().GetWindowWidth() * 0.5F,Game::GetInstance().GetWindowHeight() * 0.5F});
-    AddObject(bedroom);
+    GameObject* bg = new GameObject();
+    bg->AddComponent(new Sprite(*bg, "resources/img/blackBG.jpg"));
+    bg->box.SetCenter({Game::GetInstance().GetWindowWidth() * 0.5F,Game::GetInstance().GetWindowHeight() * 0.5F});
+    bg->AddComponent(new CameraFollower(*bg));
+    AddObject(bg);
+
+    // keep as game object 1 of bakeryState
+    GameObject* room = new GameObject();
+    room->AddComponent(new Sprite(*room, "resources/img/bedroom/bedroom.png"));
+    room->box.SetCenter({Game::GetInstance().GetWindowWidth() * 0.5F,Game::GetInstance().GetWindowHeight() * 0.5F});
+    AddObject(room);
+
 
     GameObject* tuki = new GameObject();
     tuki->AddComponent(new TukiB(*tuki));
@@ -19,6 +29,15 @@ BakeryState::BakeryState() : State() {
 
     GameData::playerAlive = true;
     backGroundMusic = std::make_unique<Music>("resources/music/MusicWorld.flac");
+
+    GameObject* speakBalloon = new GameObject();
+    Sprite* sprite = new Sprite(*speakBalloon, "resources/img/MenuButton.png");
+    speakBalloon->AddComponent(sprite);
+    sprite->SetScale(3.0f, 2.5f);
+    speakBalloon->box.x = 320;
+    speakBalloon->box.y = 570;
+
+    //AddObject(speakBalloon);
 }
 
 BakeryState::~BakeryState() {
@@ -30,15 +49,42 @@ void BakeryState::LoadAssets() {
 }
 
 void BakeryState::Update(float dt) {
-    InputManager& input = InputManager::GetInstance();
+    InputManager& iM = InputManager::GetInstance();
 
-    if (input.KeyPress(ESCAPE_KEY) || input.QuitRequested()) {
-        quitRequested = true;
-        backGroundMusic->Stop(50);
+     if (iM.KeyPress(ESCAPE_KEY) || iM.QuitRequested() || iM.KeyPress(P_KEY)){
+        shadowObj = new GameObject();        
+        Sprite* shadow = new  Sprite(*shadowObj,"resources/img/Shadow.png");
+        shadow->SetAlpha(128);
+        shadowObj->box.x = 0;
+        shadowObj->box.y = 0;
+        shadowObj->AddComponent(shadow);
+        AddObject(shadowObj);
+        
+        ResumeState* newState = new ResumeState();
+        Game::GetInstance().Push(newState);
+    }
+    if(GameData::quitOWState){
+        popRequested = true; 
+        GameData::quitOWState = false;
     }
 
-    if (((TukiB*)Player::player)->GetFloor() != floor) {
-        floor = ((TukiB*)Player::player)->GetFloor();
+    // changes floor if Tuki goes down/up stairs
+    int newFloor = ((TukiB*)Player::player)->GetFloor();
+    if (newFloor != floor) {
+        floor = newFloor;
+        objectArray[1]->RemoveComponent(objectArray[1]->GetComponent("Sprite").lock().get());
+        if (floor == 0) 
+            objectArray[1]->AddComponent(new Sprite(*objectArray[1], "resources/img/bedroom/bedroom.png"));
+        else if (floor == 1)
+            objectArray[1]->AddComponent(new Sprite(*objectArray[1], "resources/img/bakery_test.png"));
+        objectArray[1]->box.SetCenter({Game::GetInstance().GetWindowWidth() * 0.5F,Game::GetInstance().GetWindowHeight() * 0.5F});
+        if (floor == 2) {
+            OverworldState* overworld = new OverworldState();
+            Game::GetInstance().Push(overworld);
+            popRequested = true;
+            GameData::quitOWState = false;
+            backGroundMusic->Stop(50);
+        }
     }
 
     UpdateArray(dt);
@@ -62,5 +108,9 @@ void BakeryState::Pause() {
 }
 
 void BakeryState::Resume() {
+    if(shadowObj){
+        RemoveObject(shadowObj);
+        shadowObj = nullptr;
+    }
     backGroundMusic->Play();
 }
