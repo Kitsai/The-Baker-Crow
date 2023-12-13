23979dd4eb23f617f4f6ff8cd1a1afce9e862a91
Lucas Rocha <102306509+Kitsai@users.noreply.github.com>
2023-12-13 13:03:32 -0300

    Camera agora respeita limite do mapa

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -20,6 +20,11 @@ void Camera::Update(float dt) {
         Game& game = Game::GetInstance();
         pos = focus->box.GetCenter() - (Vec2(game.GetWindowWidth(),game.GetWindowHeight())*0.5F);
 
+        if(pos.x < 0) pos.x = 0;
+        else if(pos.x > 3840) pos.x = 3840;
+        if(pos.y < 0) pos.y = 0;
+        else if(pos.y > 2160) pos.y = 2160;
+
     } else {
 
     }
